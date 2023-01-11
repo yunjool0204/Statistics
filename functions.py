5d3df9aa83e0c5f9f9999d45f48568176ef77c17
yunjool0204 <yunjool0204@gmail.com>
2023-01-10 21:19:28 -0500

    Add 'getFilmPermitData_csv' function. Change the name of 'getFilmPermitData' to ''getFilmPermitData_API'

--- functions.py
+++ functions.py
@@ -31,7 +31,7 @@ def getWeatherByZipCode(zipCode):
 
     return result
 
-def getFilmPermitData():
+def getFilmPermitData_API():
     MyAPPToken = os.environ.get('Soc_App_token')
     nyc_username = os.environ.get('nyc_username')
     nyc_password = os.environ.get('nyc_password')
@@ -41,7 +41,8 @@ def getFilmPermitData():
                      username=nyc_username,
                      password=nyc_password)
 
-    # Returned as JSON from API / converted to Python list of dictionaries by sodapy.
+    # First 2000 results, returned as JSON from API / converted to Python list of
+    # dictionaries by sodapy.
     results = client.get("tg4x-b46p")
     #results = client.get("tg4x-b46p", limit=20)
 
@@ -73,8 +74,44 @@ def getFilmPermitData():
 
     return results_df
 
-#In progress
-def getCategoryChart(df): 
+
+def getFilmPermitData_csv(csv_file):
+    # Download a csv file from https://data.cityofnewyork.us/City-Government/Film-Permits/tg4x-b46p
+    # Convert to pandas DataFrame
+    results_df = pd.read_csv(csv_file)
+
+    # Identify the year and month of the permit start date.
+    results_df["StartYear"] = results_df["StartDateTime"].str[:4]
+    results_df["StartMonth"] = results_df["StartDateTime"].str[6:7]
+
+    # Split multiple zipcodes into multiple columns
+    results_df["ZipCode(s)"] = results_df["ZipCode(s)"].astype(str)
+    results_df["ZipCodeLength"] = results_df["ZipCode(s)"].str.len()
+
+    # Identify the longest zipcode list among the permits
+    series = results_df["ZipCodeLength"]
+    max = series.max()
+    id = series.idxmax()
+
+    # Identify the maximum number of zipcode in one array and determine the number of columns related to zipcodes.
+    # e.x) If  one event happened in zipcodes [10019, 10020, 10022, 10036, 10112, 11201],
+    # the length of the array is 40 = 7*5+5 (7 includes 5 zipcode letters, comma, and space)
+    # The number of the zipcodes becomes 6 = (40-5)/7 + 1.
+    zip_idx = (max - 5) / 7 + 1
+    zip_idx = int(zip_idx)
+    for i in range(zip_idx):
+        new_col = f"zipcode_{i}"
+        results_df[new_col] = results_df["ZipCode(s)"].str.split(pat=',', expand=True).iloc[:, i]
+
+    # Rename often used columns to be consistent with the API data.
+    results_df.rename(columns={'Category': 'category'}, inplace=True)
+    results_df.rename(columns={'Borough': 'borough'}, inplace=True)
+
+    return results_df
+
+
+
+def getCategoryChart(df):
     category_ct = pd.DataFrame(df["category"].value_counts())
     category_ct = category_ct.reset_index()
     category_ct.columns = ['category', 'counts']
@@ -87,13 +124,48 @@ def getCategoryChart(df):
     plt.pie(data=category_ct["counts"], labels=category_ct["category"], colors=colors, autopct='%.0f%%')
     plt.show()
 
-#In progress
+
 def getBoroughChart(df):
     sns.histplot(data=df, x="borough", shrink=.8, alpha=.8, legend=False)
     plt.show()
 
-#In progress
 def my_plot(x, y, style='o--', label='Data'):
     f, ax = plt.subplots()
     ax.plot(x, y, style, label=label)
     return ax
+
+x = [1, 2, 3]
+y1 = [10, 20, 30]
+ax = my_plot(x, y1, label='y1')
+plt.legend()
+plt.grid()
+plt.show()
+
+# def getFilmPermitData_byYr(startdate, enddate):
+#     nyc_url = f"https://data.cityofnewyork.us/resource/tg4x-b46p.json?$where=startdatetime between {startdate} and {enddate}"
+#     response = requests.get(nyc_url)
+#     opennycData = json.loads(response.content)
+#     return opennycData_df = pd.DataFrame.from_records(opennycData)
+
+#print(results_df["startdatetime"])
+ # url = "https://data.cityofnewyork.us/resource/tg4x-b46p.json?startdatetime=2021-01-04T00:01:00.000"
+ # response = requests.get(url)
+ # opennycData = json.loads(response.content)
+ # opennycData_df = pd.DataFrame.from_records(opennycData)
+ # print(opennycData_df)
+ # startdate = "'2022-01-01T00:00:00'"
+ # enddate = "2022-12-31T00:00:00"
+ # #opennycData_df = getFilmPermitData_byYr(startdate, enddate)
+ #
+ # url = f"https://data.cityofnewyork.us/resource/tg4x-b46p.json?$where=startdatetime between {startdate} and {enddate}"
+ # response = requests.get(url)
+ # opennycData = json.loads(response.content)
+ # opennycData_df = pd.DataFrame.from_records(opennycData)
+ # print(opennycData_df)
+
+
+
+
+
+
+
